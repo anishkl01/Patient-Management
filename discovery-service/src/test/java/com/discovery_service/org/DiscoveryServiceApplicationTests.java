aa2eea371f4c9467c8e006b7576d7499329945fb
anishkl01 <anishkulal777@gmail.com>
2025-05-02 14:20:44 +0530

    discovery server for registering microservices

---
+++ discovery-service/src/test/java/com/discovery_service/org/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discovery_service.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
